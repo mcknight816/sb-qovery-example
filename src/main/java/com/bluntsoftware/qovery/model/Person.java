d0193eb7eee8e7625b4da0e0c8b16b34a80cb124
mcknight816 <mcknight816@gmail.com>
2021-08-17 08:29:23 -0400

    Update Person.java

--- src/main/java/com/bluntsoftware/qovery/model/Person.java
+++ src/main/java/com/bluntsoftware/qovery/model/Person.java
@@ -1,1 +1,18 @@
+package com.bluntsoftware.qovery.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+  @Id
+  String id;
+  String firstName;
+  String lastName;
+  Integer age;
+}
